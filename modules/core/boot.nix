4365b6c3d77d33b01dfd776fd89727ada79f3858
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2026-04-07 18:26:00 +0300

    Add kernelParams to boot.nix

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,10 +1,24 @@
 { inputs, ... }:
 
 {
-  flake.modules.nixos.core = {
+  flake.modules.nixos.core = { config, lib, ... }: let
+    iStrix = config.networking.hostName == "Strix";
+  in {
     imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
 
     boot = {
+      kernelParams =
+        [
+          "splash"
+          "quiet"
+          "loglevel=3"
+          "systemd.show_status=auto"
+          "rd.systemd.show_status=false"
+          "rd.udev.log_level=3"
+          "udev.log_priority=3"
+        ]
+        ++ lib.optional iStrix "i8042.nokbd";
+
       initrd.systemd.enable = true;
       bootspec.enable = true;
       loader = {

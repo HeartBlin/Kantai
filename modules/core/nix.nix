208ffe408bfcc8bb9e89992878fd42f3f1d49f6e
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2026-05-12 15:30:09 +0300

    ci: Use GHA to check flake

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -53,16 +53,19 @@
       substituters = [
         "https://cache.nixos.org"
         "https://hyprland.cachix.org"
+        "https://kantai.cachix.org"
       ];
 
       trusted-substituters = [
         "https://cache.nixos.org"
         "https://hyprland.cachix.org"
+        "https://kantai.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "kantai.cachix.org-1:ldVeeaAtkCZs7BUSdLscSem+dX9wtqCT8cpks3HMFMk="
       ];
     };
   };
